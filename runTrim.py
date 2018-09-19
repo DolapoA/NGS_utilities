1c0531ae9b94f1c1d6ba943da519fe2556055cd8
DolapoA <dolaajayi@hotmail.com>
2018-09-19 10:58:07 +0100

    Update runTrim.py

--- runTrim.py
+++ runTrim.py
@@ -16,7 +16,7 @@ newdataPath=os.path.join(dataPath, runName+"_trimmed")
 
 
 def data_prep():
-	trim_ans=raw_input("Enter 'P' for paired end sequences OR \nEnter 'U' for unpaired end sequences: \n ")
+	trim_ans=raw_input("\nEnter 'P' for paired end sequences OR \nEnter 'U' for unpaired end sequences: OR \nEnter 'B' to keep both: \n")
 
 	print("\nTrimming raw fastq files in: " + dataPath)
 	print("\n")
@@ -42,21 +42,26 @@ def data_prep():
 
 		prep_command=" ".join(["java -jar /opt/Trimmomatic-0.36/trimmomatic-0.36.jar PE -phred33 -threads 16", R1, R2, R1P, R1U, R2P, R2U, "".join(["ILLUMINACLIP:", adapter, ":2:30:10"]), "LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36"])
 		subprocess.check_call(prep_command, shell=True)
-	if 'P' in trim_ans.upper():    
+	if trim_ans.upper() != 'B':
+		if 'P' in trim_ans.upper():    
+			for i in os.listdir(newdataPath):
+				if '_U_' in i:
+					os.remove(i)
+		elif 'U' in trim_ans:
+			for i in os.listdir(newdataPath):
+				if '_P_' in i:
+					os.remove(i)
+		elif trim_ans.upper() != 'P' and trim_ans.upper() != 'U':
+			for i in os.listdir(newdataPath):
+				if '_U_' in i:
+					os.remove(i)
 		for i in os.listdir(newdataPath):
-			if '_U_' in i:
+			if '_P_' not in i and '_U_' not in i:
 				os.remove(i)
-	elif 'U' in trim_ans:
+	else:
 		for i in os.listdir(newdataPath):
-			if '_P_' in i:
+			if '_P_' not in i and '_U_' not in i:
 				os.remove(i)
-	elif trim_ans.upper() != 'P' and trim_ans.upper() != 'U':
-		for i in os.listdir(newdataPath):
-			if '_U_' in i:
-				os.remove(i)
-	for i in os.listdir(newdataPath):
-		if '_P_' not in i and '_U_' not in i:
-			os.remove(i)
 
 if dataPath.lower()=='-h' or dataPath.lower()=='--help':
 	print("\nRun script as follows: \n python "+scriptPath+"/run_trim.py      /path/to/dataset\n")
